c547354da6360a47a7b26cdecfe535ae62545091
Kulpot <kulpotwahu@gmail.com>
2023-04-08 22:47:02 +0800

    Add project files.

---
+++ GraphicsFillDrawExtMethod/ExtensionMethods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicsFillDrawExtMethod
+{
+    static class ExtensionMethods
+    {
+        public static void FillPlusSign(this Graphics graphics, Brush brush, RectangleF rect, float thickness)
+        {
+            float smallestDim = (rect.Width < rect.Height) ? rect.Width : rect.Height;
+
+            // Draw vertical rectangle
+            //float x = rect.X + rect.Width / 2 - thickness / 2;
+            float x = rect.X + smallestDim / 2 - thickness / 2;
+            //graphics.FillRectangle(brush, x, rect.Y, thickness, rect.Height);
+            graphics.FillRectangle(brush, x, rect.Y, thickness, smallestDim);
+
+            // Draw horizontal rectangle
+            //float y = rect.Y + rect.Height / 2 - thickness / 2;
+            float y = rect.Y + smallestDim / 2 - thickness / 2;
+            //graphics.FillRectangle(brush, rect.X, y, rect.Width, thickness);
+            graphics.FillRectangle(brush, rect.X, y, smallestDim, thickness);
+        }
+    }
+}
---
+++ GraphicsFillDrawExtMethod/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphicsFillDrawExtMethod
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Rectangle rect = this.ClientRectangle;
+            rect.Inflate(-10, -10);
+            e.Graphics.FillPlusSign(Brushes.Green, rect, 30);
+        }
+    }
+}
